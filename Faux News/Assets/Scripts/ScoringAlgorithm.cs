da035bba43e18a0e2ee43c56690593d9c9e68a1c
Lyzzi Brooks <brookse@msoe.edu>
2015-04-19 14:40:38 -0500

    stuff for calculatin

--- Faux News/Assets/Scripts/ScoringAlgorithm.cs
+++ Faux News/Assets/Scripts/ScoringAlgorithm.cs
@@ -5,8 +5,12 @@ public class ScoringAlgorithm : MonoBehaviour {
 
 	double storyScore;
 	static double totalScore;
+	int totalRating;
+	double totalCredibility;
 
 	void CalculateScore(int rating, double credibility){
+		totalRating += totalRating + rating;
+		totalCredibility += totalCredibility + credibility;
 		storyScore = rating * credibility;
 		totalScore += totalScore = storyScore;
 	}
