89163908cad820309cf1b26f38377e39996061d0
Lyzzi Brooks <brookse@msoe.edu>
2015-04-19 16:02:40 -0500

    stuff for week wrap up scene

--- Faux News/Assets/Art/NextWeekScreen/NextWeekScript.js
+++ Faux News/Assets/Art/NextWeekScreen/NextWeekScript.js
@@ -1,7 +1,13 @@
 ﻿#pragma strict
 
-function Start () {
+public var ratingAndCred : GUIText;
+public var weeksEvents : GUIText;
+public var weekScore : GUIText;
 
+function Start () {
+	ratingAndCred.text = "This week's ratings: stuff\n\nThis week's credibility score: things";
+	weeksEvents.text = "Important events this week:\n\t - Kanye West does stuff";
+	weekScore.text = "WEEK X SCORE: LOTS";
 }
 
 function Update () {
